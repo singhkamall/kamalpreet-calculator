7c77899fa79b9b7387a7c5e83ac698f99f547f5f
Kamalpreet Singh <kamalpreetsingh@Kamalpreets-Mac.local>
2017-09-26 12:58:15 -0700

    binary operation implemented

--- Kamal-Calculator/ViewController.swift
+++ Kamal-Calculator/ViewController.swift
@@ -28,6 +28,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    private enum Operation{
+        case binary((Double, Double) -> Double, (String, String) -> String)
+        case equals
+    }
+    
+    private var operations:Dictionary<String,Operation>=[
+        "x": Operation.binary(*, { $0 + "x" + $1 }),
+        "/": Operation.binary(/, { $0 + "/" + $1 }),
+        "+": Operation.binary(+, { $0 + "+" + $1 }),
+        "-": Operation.binary(-, { $0 + "-" + $1 }),
+        "=": Operation.equals,
+    ]
 
     @IBAction func btnNum(_ sender: UIButton) {
         let digit = sender.currentTitle!
@@ -58,7 +70,97 @@ class ViewController: UIViewController {
     }
     
     @IBAction func btnOperators(_ sender: UIButton) {
+        if isUserTyping{
+            setOperand(displayValue)
+            isUserTyping=false
+        }
+        if let mathSymbol = sender.currentTitle{
+            performOperation(mathSymbol)
+        }
+        
+        displayResult()
     }
+    
+    private func displayResult(){
+        let eval = evaluate()
+        if let result = eval.result{
+            
+        displayValue = result
+        }
+        
+    }
+    
+    func evaluate(using variables: Dictionary<String,Double>? = nil)
+        -> (result: Double?, isPending: Bool, description: String, error: String?)
+    {
+        var accumulator: (Double, String)?
+        
+        var pendingBinaryOperation: PendingBinaryOperation?
+        
+        var error: String?
+        
+        struct PendingBinaryOperation {
+            let symbol: String
+            let function: (Double, Double) -> Double
+            let description: (String, String) -> String
+            let firstOperand: (Double, String)
+            
+            func perform(with secondOperand: (Double, String)) -> (Double, String) {
+                return (function(firstOperand.0, secondOperand.0), description(firstOperand.1, secondOperand.1))
+            }
+        }
+        
+        func performPendingBinaryOperation() {
+            if nil != pendingBinaryOperation && nil != accumulator {
+                accumulator = pendingBinaryOperation!.perform(with: accumulator!)
+                pendingBinaryOperation = nil
+            }
+        }
+        
+        var result: Double? {
+            if nil != accumulator {
+                return accumulator!.0
+            }
+            return nil
+        }
+        
+        var description: String? {
+            if nil != pendingBinaryOperation {
+                return pendingBinaryOperation!.description(pendingBinaryOperation!.firstOperand.1, accumulator?.1 ?? "")
+            } else {
+                return accumulator?.1
+            }
+        }
+        
+        for element in stack {
+            switch element {
+            case .operand(let value):
+                accumulator = (value, "\(value)")
+            case .operation(let symbol):
+                if let operation = operations[symbol] {
+                    switch operation {
+                    case .binary(let function, let description):
+                        performPendingBinaryOperation()
+                        if nil != accumulator {
+                            pendingBinaryOperation = PendingBinaryOperation(symbol: symbol, function: function, description: description, firstOperand: accumulator!)
+                            accumulator = nil
+                        }
+                    case .equals:
+                        performPendingBinaryOperation()
+                    }
+                }
+            case .variable(let symbol):
+                if let value = variables?[symbol] {
+                    accumulator = (value, symbol)
+                } else {
+                    accumulator = (0, symbol)
+                }
+            }
+        }
+        
+        return (result, nil != pendingBinaryOperation, description ?? "", error)
+    }
+
     
     @IBAction func btnCancel(_ sender: UIButton) {
         displayValue = 0
@@ -75,5 +177,19 @@ class ViewController: UIViewController {
         }
     }
     
-    
+    private var stack = [Element]()
+    private enum Element{
+        case operation(String)
+        case operand(Double)
+        case variable(String)
+    }
+    func setOperand(_ operand: Double){
+        stack.append(Element.operand(operand))
+    }
+    func setOperand(variable named: String){
+        stack.append(Element.variable(named))
+    }
+    func performOperation(_ symbol:String){
+        stack.append(Element.operation(symbol))
+    }
 }
